32fd7534c19102bb57806a1a8e7f73d7f0397d29
Aleksander <51536161+akozdev@users.noreply.github.com>
2020-02-07 12:49:52 +0100

    Includes transforming of deeplink

--- script.py
+++ script.py
@@ -3,18 +3,22 @@ file_with_new_links = open('./.data/new_links.txt', 'w')
 file_with_instructor_links = open('./.data/instructor_links.txt', 'w')
 
 for link in file_with_old_links:
+
+    # It's impossible to generate the link to Q&A from the deep link (no info about the course name)
+    if 'deeplink' in link:
+        instructor_link = link.split('/?')[0]
+        file_with_instructor_links.write(f'{instructor_link}\n')
+        continue
+
     # Get the question id from the old link
     question_id = link.split('/')[-1].rstrip()
     course_name = link.split('/')[3]
 
-    # Prevent from transforming already correct links
     if course_name == 'course':
         new_link = link.rstrip()
     else:
-        # Build a new link
         new_link = f'https://www.udemy.com/course/{course_name}/learn/lecture/5869204#questions/{question_id}'
 
-    # Build the instructor link
     instructor_link = f'https://www.udemy.com/instructor/communication/qa/{question_id}/detail/'
 
     # Write the new link to a file

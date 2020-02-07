3f5e5e413ceabee84a65ca8d150f464185bec5b7
Aleksander <51536161+akozdev@users.noreply.github.com>
2020-02-07 10:45:22 +0100

    Remove unnecessary code

--- script.py
+++ script.py
@@ -7,11 +7,14 @@ for link in file_with_old_links:
     question_id = link.split('/')[-1].rstrip()
     course_name = link.split('/')[3]
 
+    # Prevent from transforming already correct links
     if course_name == 'course':
         new_link = link.rstrip()
     else:
+        # Build a new link
         new_link = f'https://www.udemy.com/course/{course_name}/learn/lecture/5869204#questions/{question_id}'
 
+    # Build the instructor link
     instructor_link = f'https://www.udemy.com/instructor/communication/qa/{question_id}/detail/'
 
     # Write the new link to a file
@@ -21,7 +24,3 @@ for link in file_with_old_links:
 file_with_old_links.close()
 file_with_new_links.close()
 file_with_instructor_links.close()
-
-# Build a new link with question id
-# new_link = f'https://www.udemy.com/course/{course_name}/learn/lecture/5869204#questions/{question_id}'
-# instructor_link = f'https://www.udemy.com/instructor/communication/qa/{question_id}/detail/'
